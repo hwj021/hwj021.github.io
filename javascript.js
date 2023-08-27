26fc783f5b8bf07cc3dc1e932453ab1bfcb339dc
hwj021 <96523209+hwj021@users.noreply.github.com>
2023-08-28 01:16:37 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -33,3 +33,41 @@ hide.addEventListener('click', () => {
                           drei.style.opacity = '1';}, 1500);
     }
 });
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+  
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = currentTime;
+  }
+  
+
+setInterval(updateClock, 1000);
+  
+
+
+  function displayMessage() {
+    const now = new Date();
+    const currentHour = now.getHours();
+  
+    let message = '';
+  
+    if (currentHour >= 0 && currentHour < 12) {
+      message = 'prob sleeping or playing valo';
+    } else if (currentHour >= 8 && currentHour < 16) {
+      message = 'in school';
+    } else if (currentHour >= 16 && currentHour < 24) {
+      message = 'feel free to text me';
+    } 
+  
+    document.getElementById('message').textContent = message;
+  }
+  
+  // Wywołujemy funkcję, aby wyświetlić początkową wiadomość
+  displayMessage();
+  
+  // Aktualizujemy wiadomość co minutę
+  setInterval(displayMessage, 60000);
